d4586060ce16697a091ec8c8db74987f673a82ca
wemali05 <alfredwemali05@gmail.com>
2019-11-12 11:37:37 +0300

    message correction

--- resources/views/layouts/_message.blade.php
+++ resources/views/layouts/_message.blade.php
@@ -1,6 +1,6 @@
 @if(session('success'))
 <div class="alert alert-success alert-dismissible fade show" role="alert">
-    <strong>Holy guacamole!</strong> {{ session('success' )}}
+    <strong>Success!</strong> {{ session('success' )}}
     <button type="button" class="sucess" data-dismiss="alert" aria-label="Close">
         <span aria-hidden="true">&times;</span>
     </button>
