c06efb02de74a871fc1b77071a45b9d847770ccd
wemali05 <alfredwemali05@gmail.com>
2019-11-12 13:53:07 +0300

    Show individual questions

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -2,8 +2,9 @@
 
 namespace App\Providers;
 
-use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use App\Question;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 
 class RouteServiceProvider extends ServiceProvider
 {
@@ -23,7 +24,9 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Route::bind('slug', function ($slug) {
+            return   Question::where('slug', $slug)->first() ?? abort(404);
+        });
 
         parent::boot();
     }
--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,5 @@ Auth::routes(['verify' => true]);
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::resource('questions', 'QuestionController');
+Route::resource('questions', 'QuestionController')->except('show');
+Route::get('/questions/{slug}', 'QuestionController@show')->name('questions.show');
