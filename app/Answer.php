701565e41d02cb3a45d9b04fbd7318d3a5f3c848
wemali05 <alfredwemali05@gmail.com>
2019-11-14 11:58:12 +0300

    update best_answer_id using eloquent event

--- app/Answer.php
+++ app/Answer.php
@@ -37,7 +37,17 @@ class Answer extends Model
         });
 
         static::deleted(function ($answer) {
-            $answer->question->decrement('answers_count');
+            $question = $answer->question;
+            $question->decrement('answers_count');
+            if ($question->best_answer_id === $answer->id) {
+                $question->best_answer_id = null;
+                $question->save();
+            }
         });
+    }
+
+    public function getStatusAttribute()
+    {
+        return $this->id === $this->question->best_answer_id ? 'vote-accepted' : '';
     }
 }
