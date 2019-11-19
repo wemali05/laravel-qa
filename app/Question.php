dfab233b7e942d6de7e480815d36ea72f0fcfb1b
wemali05 <alfredwemali05@gmail.com>
2019-11-19 23:30:41 +0300

    Rewrite favorite button into vue js component

--- app/Question.php
+++ app/Question.php
@@ -9,7 +9,7 @@ class Question extends Model
 {
     protected $fillable = [ 'title', 'body' ];
 
-    protected $appends = ['created_date'];
+    protected $appends = ['created_date', 'is_favorited', 'favorites_count'];
 
     public function user()
     {
--- resources/views/shared/_vote.blade.php
+++ resources/views/shared/_vote.blade.php
@@ -34,9 +34,7 @@ $formAction = "/{$firstURISegment}/{$model->id}/vote";
         <input type="hidden" name="vote" value="-1">
     </form>
     @if($model instanceof App\Question)
-    @include('shared._favorite', [
-    'model' => $model
-    ])
+    <favorite :question="{{ $model }}"></favorite>
     @elseif($model instanceof App\Answer)
     @include('shared._accept', [
     'model' => $model
