12467be4ecfb88041d91d64418d04973e032c892
wemali05 <alfredwemali05@gmail.com>
2019-11-26 18:27:22 +0300

    Create endpoints for answer resources

--- routes/api.php
+++ routes/api.php
@@ -17,9 +17,11 @@ Route::post('/token', 'Auth\loginController@getToken');
 Route::get('/questions', 'Api\QuestionsController@index');
 
 Route::get('/questions/{question}-{slug}', 'Api\QuestionDetailsController');
+Route::get('/questions/{question}/answers', 'Api\Answerscontroller@index');
 
 Route::middleware(['auth:api'])->group(function () {
     Route::apiResource('/questions', 'Api\QuestionsController')->except('index');
+    Route::apiResource('/questions.answers', 'Api\AnswersController')->except('index');
 });
 
 
